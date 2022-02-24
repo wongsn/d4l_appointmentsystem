e2aa0ee4b2f5384ad38a866e19aea0cddea75351
Shen Nan <wongshennan@live.com>
2022-02-24 09:58:41 +0800

    edit: nextconfig to include env fallbacks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_HASURA_URI: process.env.NEXT_PUBLIC_HASURA_URI,
+    NEXT_PUBLIC_HASURA_ADMIN_KEY: process.env.NEXT_PUBLIC_HASURA_ADMIN_KEY
+  }
 }
 
 module.exports = nextConfig
